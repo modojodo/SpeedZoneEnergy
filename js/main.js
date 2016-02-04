fc0e425a9ce6f386363898565b8d2dd4efc88594
Umer Hassan <umerhassan94@gmail.com>
2016-02-04 17:46:49 -0600

    Slick needs to be removed and add the owl carousel. Second section needs to be done properly

--- js/main.js
+++ js/main.js
@@ -11,6 +11,24 @@ $(function () {
         windowHeight = $(window).height() - navbarHeight;
         $('.section').height(windowHeight);
     });
+    //$('.slick-center img').width(150);
+    //$('#slick').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    //
+    //    //$('.slick-center img').animate({
+    //    //    width: 150
+    //    //}, 100);
+    //});
+    $('#slick').on('afterChange', function (event, slick, currentSlide, nextSlide) {
 
+        //$('.slick-active img').animate({
+        //    width: 100
+        //}, 100);
+        //$('.slick-center img').animate({
+        //    width: 200
+        //    }, 100);
+        $('.slick-active img').removeClass('animated bounce');
+        $('.slick-center img').addClass('animated bounce');
+        //$('.slick-center img').addClass('animated flash');
 
+    });
 });
