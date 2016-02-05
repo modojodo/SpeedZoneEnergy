a1ce4746a656352611df0f067d460abd17764e78
Umer Hassan <umerhassan94@gmail.com>
2016-02-05 17:13:41 -0600

    The second section is done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $(function () {
 
 
 //  initializing jQuery backstretch
-    $.backstretch("../img/lambo.jpg");
+    $.backstretch(["../img/lambo.jpg", "../img/summer.jpg"], {fade: 1000});
+    $.backstretch('pause');
+
     var windowHeight;
     var navbarHeight = 40; //This is the padding that is required in the css file
     windowHeight = $(window).height() - navbarHeight;
@@ -11,24 +13,35 @@ $(function () {
         windowHeight = $(window).height() - navbarHeight;
         $('.section').height(windowHeight);
     });
-    //$('.slick-center img').width(150);
+    $('#product').waypoint(function (direction) {
+        if (direction === 'down') {
+            $.backstretch('next');
+        } else {
+            $.backstretch('prev');
+        }
+
+    }, {
+        offset: '25%'
+    })
+
+
+    //$('.slick-center img').width(180);
     //$('#slick').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    //console.log("BeforeChange");
+    //console.log("Currrent Slide:  " + currentSlide);
+    //console.log("Next Slide:  " + nextSlide);
     //
-    //    //$('.slick-center img').animate({
-    //    //    width: 150
-    //    //}, 100);
+    //console.log($('.slick').children().eq(currentSlide).html());
+    //console.log("Currrent Slide:  " + $("[data-slick-index='" + currentSlide + "']").html());
+    //console.log("Next Slide:  " + $("[data-slick-index='" + (nextSlide) + "']").html());
+    //console.log("Previous Slide:  " + $("[data-slick-index='" + (currentSlide - 1) + "']").html());
+    //$("[data-slick-index='" + currentSlide + "']").find('img').width(100);
+    //$("[data-slick-index='" + nextSlide + "']").find('img').width(180);
     //});
-    $('#slick').on('afterChange', function (event, slick, currentSlide, nextSlide) {
 
-        //$('.slick-active img').animate({
-        //    width: 100
-        //}, 100);
-        //$('.slick-center img').animate({
-        //    width: 200
-        //    }, 100);
-        $('.slick-active img').removeClass('animated bounce');
-        $('.slick-center img').addClass('animated bounce');
-        //$('.slick-center img').addClass('animated flash');
-
-    });
+    //$('#slick').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+    //    console.log("AfterChange");
+    //    console.log("Currrent Slide:  " + currentSlide);
+    //    console.log("Next Slide:  " + nextSlide);
+    //});
 });
