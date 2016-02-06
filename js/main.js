d85164aba1edddf2d560c0d6fc2cac5657d6da04
Umer Hassan <umerhassan94@gmail.com>
2016-02-06 16:52:39 -0600

    Added third section and fixed padding issue

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ $(function () {
 
 
 //  initializing jQuery backstretch
-    $.backstretch(["../img/lambo.jpg", "../img/summer.jpg"], {fade: 1000});
+    $.backstretch(["../img/lambo.jpg", "../img/summer.jpg", "../img/bmw.jpg"], {fade: 1000});
     $.backstretch('pause');
     $('#fullpage').fullpage({
         anchors: ['home-page', 'products-page'],
         menu: '#menu',
+        scrollOverflow: true,
         onLeave: function (index, nextIndex, direction) {
 
             //// Make navbar active after leaving 1st section
